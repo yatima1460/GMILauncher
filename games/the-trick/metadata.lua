605b412d64825254df3effe256fd20b943e826f8
Yatima Santamorena <yatima@santamorena.me>
2025-11-12 15:59:43 +0100

    add the-trick game

---
+++ games/the-trick/metadata.lua
@@ -0,0 +1,8 @@
+return {
+    title = "The Trick",
+    version = "2020_05_13",
+    author = "AlexoFalco",
+    url = "https://www.indiexpo.net/it/games/the-trick-virus-attack",
+    exe = "TheTrick.exe",
+    year = "2020"
+}
