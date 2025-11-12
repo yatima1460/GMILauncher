33c5c572ce37b5dfabda068dc14f7c9fb3a3b8bc
Yatima <yatima@santamorena.me>
2025-11-12 13:03:20 +0100

    use metadata.lua instead of json

---
+++ games/despelunca-ex-elementis/metadata.lua
@@ -0,0 +1,8 @@
+return {
+    title = "De Spelunca Ex Elementis",
+    version = "1.3.2",
+    author = "Scario",
+    url = "https://scario88.itch.io/de-spelunca-ex-elementis",
+    source = "https://github.com/davidescarioni/de-spelunca-ex-elementis",
+    exe = "De Spelunca Ex Elementis.exe"
+}
